0f726961513b6cfca889dc333e30ed8d9067d31b
Swapnil Chillalnode <srchillal589@gmail.com>
2023-06-22 21:37:46 +0530

    configured firebase installation

---
+++ src/misc/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyAV4J_GNnRCLQk7qvCapbumezm2uzysESk",
+    authDomain: "chat-web-app-7b707.firebaseapp.com",
+    databaseURL: "https://chat-web-app-7b707-default-rtdb.firebaseio.com",
+    projectId: "chat-web-app-7b707",
+    storageBucket: "chat-web-app-7b707.appspot.com",
+    messagingSenderId: "695187614776",
+    appId: "1:695187614776:web:2420263726d143ceb38acd"
+  };
+
+  const app = firebase.initializeApp(config);
+
+app.
